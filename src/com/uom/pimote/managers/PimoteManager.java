abdceccd9b188a2c9f8c44a69791d1a3d32c5c9d
Tom Richardson <tom@ajrmail.co.uk>
2013-08-13 14:44:36 +0100

    Rearrange

--- src/com/uom/pimote/managers/PimoteManager.java
+++ src/com/uom/pimote/managers/PimoteManager.java
@@ -2,9 +2,22 @@ package com.uom.pimote.managers;
 
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.text.Html;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.uom.pimote.Communicator;
+import com.uom.pimote.R;
 import com.uom.pimote.SensorManagement;
 import com.uom.pimote.TCPClient;
 import com.uom.pimote.mjpegvideo.MjpegStreamManager;
@@ -16,6 +29,14 @@ import java.util.ArrayList;
  * Created by Tom on 02/07/2013.
  */
 public class PimoteManager {
+
+    // Protocol Variables
+    public static final int SETUP = 1;
+    public static final int REQUEST_OUTPUT_CHANGE = 2;
+    // Protocol Variables to decide what components to use
+    public static final int BUTTON = 1, TEXT_INPUT = 2, TOGGLE_BUTTON = 3, TEXT_OUTPUT = 4,
+            VIDEO_FEED = 5, VOICE_INPUT = 6, RECURRING_INFO = 7, PROGRESS_BAR = 8,
+            SPACER = 9, CLEAR_ALL = 0;
 
     // Global Variables
     ArrayList<RecurringInfo> threads; // All threads
@@ -37,6 +58,189 @@ public class PimoteManager {
 
     // Called when a message is received. Overridden by manager
     public void onMessage(String[] message) {
+        switch (Integer.parseInt(message[0])) {
+            case SETUP:
+                String[] setup = new String[message.length - 1];
+                for (int i = 1; i < message.length; i++)
+                    setup[i - 1] = message[i];
+                setup(setup);
+                break;
+
+            case REQUEST_OUTPUT_CHANGE:
+                String[] info = new String[message.length - 1];
+                for (int i = 1; i < message.length; i++)
+                    info[i - 1] = message[i];
+                changeOutput(info);
+                break;
+        }
+    }
+
+    public void setup(String[] setup){}
+    public void changeOutput(String[] info){}
+
+    public String addIllegalChars(String string) {
+        return string.replace("%/", ",");
+    }
+
+    //Component type, ID
+    public Button createButton(final String[] setup) {
+        Button button = new Button(c);
+        button.setText(addIllegalChars(setup[2]));
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (tcp != null)
+                    send(setup[1] + "," + " ");
+            }
+        });
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins(10, 5, 10, 5);
+        button.setLayoutParams(params);
+        return button;
+    }
+
+    //Component type, Id, Hint
+    public LinearLayout createTextInput(final String[] setup) {
+        LinearLayout textButtonLayout = new LinearLayout(c);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 10, 0, 10);
+        textButtonLayout.setLayoutParams(params);
+        final EditText addText = new EditText(c);
+        addText.setHint(addIllegalChars(setup[2]));
+        LinearLayout.LayoutParams params2 = new TableRow.LayoutParams(0,
+                LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+        addText.setLayoutParams(params2);
+        Button button = new Button(c);
+        button.setText("Send");
+        button.setGravity(3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (tcp != null) {
+                    String text = addText.getText().toString();
+                    if (text.equals("")) text = "null";
+                    send(setup[1] + "," + text);
+                }
+                addText.setText("");
+            }
+        });
+        textButtonLayout.addView(addText);
+        textButtonLayout.addView(button);
+
+        return textButtonLayout;
+    }
+
+    //Component type, Id, Text, Initial
+    public LinearLayout createToggleButton(final String[] setup) {
+        LinearLayout textButtonLayout = new LinearLayout(c);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 10, 0, 10);
+        textButtonLayout.setLayoutParams(params);
+        TextView text = new TextView(c);
+        text.setTextSize(16);
+        text.setText(addIllegalChars(setup[2]));
+        LinearLayout.LayoutParams params2 = new TableRow.LayoutParams(0,
+                LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
+        text.setLayoutParams(params2);
+        ToggleButton button = new ToggleButton(c);
+        boolean checked = Integer.parseInt(setup[3]) == 1;
+        button.setChecked(checked);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int tf = ((ToggleButton) view).isChecked() ? 1 : 0;
+                send(setup[1] + "," + tf);
+            }
+        });
+        textButtonLayout.addView(text);
+        textButtonLayout.addView(button);
+        button.setId(Integer.parseInt(setup[1]));
+
+        return textButtonLayout;
+    }
+
+    //Component type, Id, Initial text, Text Size
+    public TextView createOutputText(final String[] setup) {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(0, 10, 0, 10);
+        TextView text = new TextView(c);
+        text.setTextSize(18);
+        text.setLayoutParams(params);
+        if (setup.length >= 3) {
+            String out = addIllegalChars(setup[2]);
+            text.setText(Html.fromHtml(out));
+        }
+        text.setTextSize(Integer.parseInt(setup[3]));
+        text.setId(Integer.parseInt(setup[1]));
+
+        return text;
+    }
+
+    //Component type, Id, Outside Ip, IP
+    public MjpegView createVideoFeed(final String[] setup, String ip) {
+        String feedIp = ip;
+        if (Integer.parseInt(setup[3]) == 1) feedIp = setup[4];
+        String URL = "http://" + feedIp + ":8080/?action=stream";
+        MjpegView mv = new MjpegView(c);
+        startVideo(mv, URL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(Integer.parseInt(setup[1]),
+                Integer.parseInt(setup[2]));
+        params.setMargins(0, 10, 0, 10);
+        mv.setLayoutParams(params);
+        mv.setVisibility(View.VISIBLE);
+
+        return mv;
+    }
+
+    //Component type, Id
+    public ImageButton createVoiceInput(final String[] setup) {
+        ImageButton voice = new ImageButton(c);
+        voice.setImageDrawable(c.getResources().getDrawable(R.drawable.mic));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100);
+        voice.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        voice.setLayoutParams(params);
+        final int id = Integer.parseInt(setup[1]);
+        voice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Communicator) c).startVoiceRecognition(id);
+            } // onClick()
+        });
+
+        return voice;
+    }
+
+    //Component type, Id, MaxValue
+    public ProgressBar createProgressBar(String[] setup) {
+        ProgressBar bar = new ProgressBar(c, null, android.R.attr.progressBarStyleHorizontal);
+        bar.setProgress(0);
+        bar.setMax(Integer.parseInt(setup[2]));
+        bar.setId(Integer.parseInt(setup[1]));
+        return bar;
+    }
+
+    //Component type, Id, Height
+    public View createSpacer(String[] setup) {
+        View v = new View(c);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, Integer.parseInt(setup[1]));
+        v.setLayoutParams(params);
+        return v;
+    }
+
+    // Returns the TextView with the matching ID
+    public TextView getTextView(int id) {
+        return (TextView) ((Communicator) c).findViewById(id);
+    }
+
+    // Returns the ProgressBar with the matching ID
+    public ProgressBar getProgressBar(int id) {
+        return (ProgressBar) ((Communicator) c).findViewById(id);
+    }
+
+    // Returns the ToggleButton with the matching ID
+    public ToggleButton getToggleButton(int id) {
+        return (ToggleButton) ((Communicator) c).findViewById(id);
     }
 
     // Start the video on a certain stream
@@ -47,26 +251,26 @@ public class PimoteManager {
     }
 
     public void stopVideo() {
-        for(MjpegStreamManager s : streams){
+        for (MjpegStreamManager s : streams) {
             s.stopVideo();
         }
     }
 
     public void pause() {
-        for(MjpegStreamManager s : streams){
+        for (MjpegStreamManager s : streams) {
             s.pauseVideo();
         }
-        if(sensors!=null)sensors.pause();
+        if (sensors != null) sensors.pause();
     }
 
     public void resume() {
-        for(MjpegStreamManager s : streams){
+        for (MjpegStreamManager s : streams) {
             s.resumeVideo();
         }
-        if(sensors!=null)sensors.resume();
+        if (sensors != null) sensors.resume();
     }
 
-    public void startSensors(int speed, TCPClient tcp){
+    public void startSensors(int speed, TCPClient tcp) {
         sensors = new SensorManagement(c, speed, tcp);
     }
 
@@ -83,17 +287,17 @@ public class PimoteManager {
             threads.get(i).stopThread();
         }
         stopVideo();
-        if(sensors!= null)sensors.pause();
+        if (sensors != null) sensors.pause();
     }
 
-    public void deleteThreads(){
+    public void deleteThreads() {
         threads.clear();
         streams.clear();
     }
 
 
     // Send a message back to the pi
-    public void send(String message){
+    public void send(String message) {
         tcp.sendMessage(Communicator.SEND_DATA + "," + message);
     }
 
--- src/com/uom/pimote/managers/RegularButtonManager.java
+++ src/com/uom/pimote/managers/RegularButtonManager.java
@@ -3,35 +3,18 @@ package com.uom.pimote.managers;
 import android.content.Context;
 import android.text.Html;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.ProgressBar;
-import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.uom.pimote.Communicator;
 import com.uom.pimote.R;
 import com.uom.pimote.TCPClient;
-import com.uom.pimote.mjpegvideo.MjpegView;
 
 import java.util.ArrayList;
 
 public class RegularButtonManager extends PimoteManager {
-    // Protocol Variables
-    private static final int SETUP = 1;
-    private static final int REQUEST_OUTPUT_CHANGE = 2;
-    // Protocol Variables to decide what components to use
-    private static final int BUTTON = 1, TEXT_INPUT = 2, TOGGLE_BUTTON = 3, TEXT_OUTPUT = 4,
-            VIDEO_FEED = 5, VOICE_INPUT = 6, RECURRING_INFO = 7, PROGRESS_BAR = 8,
-            SPACER = 9, CLEAR_ALL = 0;
     // Global Variables
     TCPClient tcp; // TCP Client for communication
     Context c; // Communicator context
@@ -60,32 +43,10 @@ public class RegularButtonManager extends PimoteManager {
     @Override
     //Receives the messages from the Pi
     public void onMessage(String[] message) {
-        switch (Integer.parseInt(message[0])) {
-            case SETUP:
-                String[] setup = new String[message.length - 1];
-                for (int i = 1; i < message.length; i++)
-                    setup[i - 1] = message[i];
-                addButtons(setup);
-                break;
-
-            case REQUEST_OUTPUT_CHANGE:
-                if (Integer.parseInt(message[1]) == TEXT_OUTPUT) {
-                    TextView output = getTextView(Integer.parseInt(message[2]));
-                    String out = addIllegalChars(message[3]);
-                    output.setText(Html.fromHtml(out));
-                } else if (Integer.parseInt(message[1]) == PROGRESS_BAR) {
-                    ProgressBar bar = getProgressBar(Integer.parseInt(message[2]));
-                    bar.setProgress(Integer.parseInt(message[3]));
-                } else if (Integer.parseInt(message[1]) == TOGGLE_BUTTON) {
-                    ToggleButton t = getToggleButton(Integer.parseInt(message[2]));
-                    t.setChecked(Integer.parseInt(message[3]) == 1);
-                }
-                break;
-        }
+        super.onMessage(message);
     }
-
-    // Add components to the screen
-    public void addButtons(final String[] setup) {
+    @Override
+    public void setup(String[] setup){
         switch (Integer.parseInt(setup[0])) {
             case CLEAR_ALL:
                 Log.e("SETUP", "Clearing");
@@ -95,197 +56,51 @@ public class RegularButtonManager extends PimoteManager {
                 deleteThreads();
                 break;
             case BUTTON:
-                addNewButton(setup);
+                layout.addView(createButton(setup), viewPosition++);
                 break;
             case TEXT_INPUT:
-                addNewTextInput(setup);
+                layout.addView(createTextInput(setup), viewPosition++);
                 break;
             case TOGGLE_BUTTON:
-                addNewToggle(setup);
+                layout.addView(createToggleButton(setup), viewPosition++);
                 break;
             case TEXT_OUTPUT:
-                addNewTextView(setup);
+                layout.addView(createOutputText(setup), viewPosition++);
                 break;
             case VIDEO_FEED:
-                addNewFeed(setup, ip);
+                layout.addView(createVideoFeed(setup, ip), viewPosition++);
                 break;
             case VOICE_INPUT:
-                addVoiceInput(setup);
+                layout.addView(createVoiceInput(setup), viewPosition++);
                 break;
             case RECURRING_INFO:
                 addRecurringInformation(Integer.parseInt(setup[1]),
                         Integer.parseInt(setup[2]), tcp);
                 break;
             case PROGRESS_BAR:
-                addProgressBar(Integer.parseInt(setup[1]), Integer.parseInt(setup[2]));
+                layout.addView(createProgressBar(setup), viewPosition++);
                 break;
             case SPACER:
-                addSpacer(Integer.parseInt(setup[1]));
+                layout.addView(createSpacer(setup), viewPosition++);
                 break;
             default:
                 Log.e("SETUP", "Unknown component");
                 break;
         }
     }
-
-    public String addIllegalChars(String string){
-        return string.replace("%/", ",");
-    }
-
-    // Adds a new button to the screen
-    public void addNewButton(final String[] setup) {
-        Button button = new Button(c);
-        button.setText(addIllegalChars(setup[2]));
-        button.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if (tcp != null)
-                    send(setup[1] + "," + " ");
-            }
-        });
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        params.setMargins(10, 5, 10, 5);
-        button.setLayoutParams(params);
-        layout.addView(button, viewPosition++);
-    }
-
-    // Adds a text input to the screen
-    public void addNewTextInput(final String[] setup) {
-        LinearLayout textButtonLayout = new LinearLayout(c);
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, 10, 0, 10);
-        textButtonLayout.setLayoutParams(params);
-        final EditText addText = new EditText(c);
-        addText.setHint(addIllegalChars(setup[2]));
-        LayoutParams params2 = new TableRow.LayoutParams(0,
-                LayoutParams.WRAP_CONTENT, 1f);
-        addText.setLayoutParams(params2);
-        Button button = new Button(c);
-        button.setText("Send");
-        button.setGravity(3);
-        button.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if (tcp != null) {
-                    String text = addText.getText().toString();
-                    if (text.equals("")) text = "null";
-                    send(setup[1] + "," + text);
-                }
-                addText.setText("");
-            }
-        });
-        textButtonLayout.addView(addText);
-        textButtonLayout.addView(button);
-        layout.addView(textButtonLayout, viewPosition++);
-    }
-
-    // Adds a ToggleButton to the screen
-    public void addNewToggle(final String[] setup) {
-        LinearLayout textButtonLayout = new LinearLayout(c);
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, 10, 0, 10);
-        textButtonLayout.setLayoutParams(params);
-        TextView text = new TextView(c);
-        text.setTextSize(16);
-        text.setText(addIllegalChars(setup[2]));
-        LayoutParams params2 = new TableRow.LayoutParams(0,
-                LayoutParams.WRAP_CONTENT, 1f);
-        text.setLayoutParams(params2);
-        ToggleButton button = new ToggleButton(c);
-        boolean checked = Integer.parseInt(setup[3]) == 1;
-        button.setChecked(checked);
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int tf = ((ToggleButton) view).isChecked() ? 1 : 0;
-                send(setup[1] + "," + tf);
-            }
-        });
-        textButtonLayout.addView(text);
-        textButtonLayout.addView(button);
-        button.setId(Integer.parseInt(setup[1]));
-        layout.addView(textButtonLayout, viewPosition++);
-    }
-
-    // Adds an output TextView to the screen
-    public void addNewTextView(final String[] setup) {
-        LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT);
-        params.setMargins(0, 10, 0, 10);
-        TextView text = new TextView(c);
-        text.setTextSize(18);
-        text.setLayoutParams(params);
-        if (setup.length >= 3) {
-            String out = addIllegalChars(setup[2]);
-            text.setText(Html.fromHtml(out));
+    @Override
+    public void changeOutput(String[] info){
+        if (Integer.parseInt(info[0]) == TEXT_OUTPUT) {
+            TextView output = getTextView(Integer.parseInt(info[1]));
+            String out = addIllegalChars(info[2]);
+            output.setText(Html.fromHtml(out));
+        } else if (Integer.parseInt(info[0]) == PROGRESS_BAR) {
+            ProgressBar bar = getProgressBar(Integer.parseInt(info[1]));
+            bar.setProgress(Integer.parseInt(info[2]));
+        } else if (Integer.parseInt(info[0]) == TOGGLE_BUTTON) {
+            ToggleButton t = getToggleButton(Integer.parseInt(info[1]));
+            t.setChecked(Integer.parseInt(info[2]) == 1);
         }
-        text.setTextSize(Integer.parseInt(setup[3]));
-        layout.addView(text, viewPosition++);
-        text.setId(Integer.parseInt(setup[1]));
-    }
-
-    // Returns the TextView with the matching ID
-    public TextView getTextView(int id) {
-        return (TextView) ((Communicator) c).findViewById(id);
-    }
-
-    // Returns the ProgressBar with the matching ID
-    public ProgressBar getProgressBar(int id) {
-        return (ProgressBar) ((Communicator) c).findViewById(id);
-    }
-
-    // Returns the ToggleButton with the matching ID
-    public ToggleButton getToggleButton(int id) {
-        return (ToggleButton) ((Communicator) c).findViewById(id);
-    }
-
-    // Add a new Video Feed (MjpgView) to the screen
-    public void addNewFeed(String[] setup, String ip) {
-        String feedIp = ip;
-        if (Integer.parseInt(setup[3]) == 1) feedIp = setup[4];
-        String URL = "http://" + feedIp + ":8080/?action=stream";
-        MjpegView mv = new MjpegView(c);
-        startVideo(mv, URL);
-        LayoutParams params = new LayoutParams(Integer.parseInt(setup[1]),
-                Integer.parseInt(setup[2]));
-        params.setMargins(0, 10, 0, 10);
-        mv.setLayoutParams(params);
-        mv.setVisibility(View.VISIBLE);
-        layout.addView(mv, viewPosition++);
-    }
-
-    // Add a voice input button to the screen
-    public void addVoiceInput(final String[] setup) {
-        ImageButton voice = new ImageButton(c);
-        voice.setImageDrawable(c.getResources().getDrawable(R.drawable.mic));
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 100);
-        voice.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        voice.setLayoutParams(params);
-        final int id = Integer.parseInt(setup[1]);
-        voice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((Communicator) c).startVoiceRecognition(id);
-            } // onClick()
-        });
-        layout.addView(voice, viewPosition++);
-    }
-
-    // Add a progress bar to the screen
-    public void addProgressBar(int id, int maxValue) {
-        ProgressBar bar = new ProgressBar(c, null, android.R.attr.progressBarStyleHorizontal);
-        bar.setProgress(0);
-        bar.setMax(maxValue);
-        bar.setId(id);
-        layout.addView(bar, viewPosition++);
-    }
-
-    // Add a spacer (blank space) to the screen
-    public void addSpacer(int size) {
-        View v = new View(c);
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, size);
-        v.setLayoutParams(params);
-        layout.addView(v, viewPosition++);
     }
 
 
